07168433fd21b7bd4e8ac5c7a56e5699767abbd6
amrit-das <amritdas1998@gmail.com>
2019-06-23 11:54:36 +0530

    asdd

--- detect.py
+++ detect.py
@@ -1,20 +1,23 @@
 import glob
 import os
-
 import torch
 from PIL import Image
 from tqdm import tqdm
+
 from ssd.config import cfg
 from ssd.data.datasets import COCODataset, VOCDataset
 from ssd.modeling.predictor import Predictor
 from ssd.modeling.vgg_ssd import build_ssd_model
+
 import argparse
 import numpy as np
+import json
 
 from ssd.utils.viz import draw_bounding_boxes
 
 
-def run_demo(cfg, weights_file, iou_threshold, score_threshold, images_dir, output_dir, dataset_type):
+def detect(cfg, weights_file, iou_threshold, score_threshold, images_dir, output_dir, dataset_type):
+    
     if dataset_type == "voc":
         class_names = VOCDataset.class_names
     elif dataset_type == 'coco':
@@ -26,14 +29,18 @@ def run_demo(cfg, weights_file, iou_threshold, score_threshold, images_dir, outp
     model = build_ssd_model(cfg)
     model.load(weights_file)
     print('Loaded weights from {}.'.format(weights_file))
+    
     model = model.to(device)
+    
     predictor = Predictor(cfg=cfg,
                           model=model,
                           iou_threshold=iou_threshold,
                           score_threshold=score_threshold,
                           device=device)
+    
     cpu_device = torch.device("cpu")
 
+    
     image_paths = glob.glob(os.path.join(images_dir, '*.jpg'))
 
     if not os.path.exists(output_dir):
@@ -50,47 +57,29 @@ def run_demo(cfg, weights_file, iou_threshold, score_threshold, images_dir, outp
 
 
 def main():
-    parser = argparse.ArgumentParser(description="SSD Demo.")
-    parser.add_argument(
-        "--config-file",
-        default="",
-        metavar="FILE",
-        help="path to config file",
-        type=str,
-    )
-    parser.add_argument("--weights", type=str, help="Trained weights.")
-    parser.add_argument("--iou_threshold", type=float, default=0.5)
-    parser.add_argument("--score_threshold", type=float, default=0.5)
-    parser.add_argument("--images_dir", default='demo', type=str, help='Specify a image dir to do prediction.')
-    parser.add_argument("--output_dir", default='demo/result', type=str, help='Specify a image dir to save predicted images.')
-    parser.add_argument("--dataset_type", default="voc", type=str, help='Specify dataset type. Currently support voc and coco.')
 
-    parser.add_argument(
-        "opts",
-        help="Modify config options using the command-line",
-        default=None,
-        nargs=argparse.REMAINDER,
-    )
-    args = parser.parse_args()
-    print(args)
-
-    cfg.merge_from_file(args.config_file)
-    cfg.merge_from_list(args.opts)
+    with open("hyper_params.json") as hp:
+        data = json.load(hp)
+        config_file = data["configfile"]
+        weights = data["weight"]
+        images_dir = data["ImgDir"]
+        output_dir = data["OutDir"]
+        iou_threshold = data["iou"]
+        score_threshold = data["score"]
+        dataset_type = data["SSD_Type"]
+        
+    opts = []
+    cfg.merge_from_file(config_file)
+    cfg.merge_from_list(opts)
     cfg.freeze()
 
-    print("Loaded configuration file {}".format(args.config_file))
-    with open(args.config_file, "r") as cf:
-        config_str = "\n" + cf.read()
-        print(config_str)
-    print("Running with config:\n{}".format(cfg))
-
-    run_demo(cfg=cfg,
-             weights_file=args.weights,
-             iou_threshold=args.iou_threshold,
-             score_threshold=args.score_threshold,
-             images_dir=args.images_dir,
-             output_dir=args.output_dir,
-             dataset_type=args.dataset_type)
+    detect(cfg=cfg,
+             weights_file=weights,
+             iou_threshold=iou_threshold,
+             score_threshold=score_threshold,
+             images_dir=images_dir,
+             output_dir=output_dir,
+             dataset_type=dataset_type)
 
 
 if __name__ == '__main__':
